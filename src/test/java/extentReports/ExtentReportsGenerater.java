b3f257b16d9608e44ee1850f2882647951f93486
Swapnil Sable <Swapnil Sable@ho.rnt.ai>
2022-06-25 19:48:45 +0530

    extenHTMLtreport in etent report

--- src/test/java/extentReports/ExtentReportsGenerater.java
+++ src/test/java/extentReports/ExtentReportsGenerater.java
@@ -4,17 +4,27 @@ import org.openqa.selenium.WebDriver;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 
-public class ExtentReportsGenerater {
 
-	ExtentReports report;
-	ExtentTest test;
-	WebDriver driver;
+public class ExtentReportsGenerater {	
 
+	public WebDriver driver;
+	public ExtentHtmlReporter htmlreporter;
+	public ExtentReports extent;
+	public ExtentTest logger;
 	public void startReport() {
 
-		report = new ExtentReports();
-
+		htmlreporter = new ExtentHtmlReporter(System.getProperty("user.dir") + "/test-output/STMExtentReport.html");
+		extent = new ExtentReports();
+		extent.attachReporter(htmlreporter);
+		extent.setSystemInfo("Swapnil sable ","Use Insider Automation");
+		extent.setSystemInfo("Envirnment=> Automation", "Staging");
+		htmlreporter.config().setDocumentTitle("Test report of Automation => use Insider");
+		htmlreporter.config().setTheme(Theme.DARK);
+		
+		
 	}
 
 }
